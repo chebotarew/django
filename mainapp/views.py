9985c4ef7e18b7c0c805e745663941f4b8f17582
Alexandr Chebotarev <chebotarev.alexandr@yandex.ru>
2019-06-04 07:24:00 +0300

    first task

---
+++ mainapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def main(request):
+    return render(request, 'mainapp/index.html')
+
+def contact(request):
+    return render(request, 'mainapp/contact.html')
+
+def about(request):
+    return render(request, 'mainapp/about.html')
