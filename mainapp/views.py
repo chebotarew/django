9e1179f048ab238b91a5c49cebd5da6effbb1aee
Alexandr Chebotarev <chebotarev.alexandr@yandex.ru>
2019-06-10 14:58:40 +0300

    second task fix

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', {'title': 'Main page'})
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', {'title': 'Contact page'})
 
 def about(request):
-    return render(request, 'mainapp/about.html')
+    return render(request, 'mainapp/about.html', {'title': 'About page'})
